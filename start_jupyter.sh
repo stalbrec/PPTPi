390205871c6a2f5c17323ab8d3fec301c6c946c4
Steffen Albrecht <steffen.alb@posteo.de>
2022-08-10 13:09:04 +0200

    Update start_jupyter.sh
    remove old comments

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -5,7 +5,6 @@ VENV=${1:-~/PPT-env}
 echo "venv:" $VENV
 
 source $VENV/bin/activate
-#jupyter-lab --ip 0.0.0.0 --no-browser --port 9999
-jupyter-lab #--ip 0.0.0.0 --no-browser --port 9999
+jupyter-lab
 
 deactivate
